a7dfa820b4f70be8e16b38c5ffc31d1bf1b3b4b8
SAQUIB-KHAN90 <saquibkhan890098@gmail.com>
2024-08-16 23:12:09 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// function runTypingEffect() {
+//   const text = "I am Saquib khan.";
+//   const typingElement = document.getElementById("typing-text"); //typing-text
+//   const typingDelay = 100;
+
+//   typeText(text, typingElement, typingDelay);
+// }
+
+// function typeText(text, typingElement, delay) {
+//   for (let i = 0; i < text.length; i++) {
+//     setTimeout(() => {
+//       typingElement.textContent += text.charAt(i);
+//     }, delay * i);
+//   }
+// }
+// document.addEventListener("DOMContentLoaded", runTypingEffect);
+function runTypingEffect() {
+  const text = "I am Saquib khan.";
+  const typingElement = document.getElementById("typing-text");
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect);
